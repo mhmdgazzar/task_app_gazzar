419839e81a6092d5290f5c3c9c43315cc9ca8710
mhmdgazzar <mhmdgazzar@gmail.com>
2024-04-17 16:51:35 +0200

    dart fix

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:task_app/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart';
 
 class S4521 extends StatelessWidget {
-  const S4521({Key? key}) : super(key: key);
+  const S4521({super.key});
   @override
   Widget build(BuildContext context) {
     return const CenterHelloWorld();
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class S4522 extends StatelessWidget {
-  const S4522({Key? key}) : super(key: key);
+  const S4522({super.key});
   @override
   Widget build(BuildContext context) {
     return const MyStatlessWidget();
