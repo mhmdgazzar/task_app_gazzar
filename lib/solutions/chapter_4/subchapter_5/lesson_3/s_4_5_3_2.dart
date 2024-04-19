3f36953d91d216a028931caa1e576e26b712eccb
mhmdgazzar <mhmdgazzar@gmail.com>
2024-04-19 14:16:31 +0200

    aufgaben

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
@@ -4,6 +4,25 @@ class S4532 extends StatelessWidget {
   const S4532({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyWidget4532();
+  }
+}
+
+class MyWidget4532 extends StatelessWidget {
+  const MyWidget4532({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('App Akademie'),
+        Text('App Akademie', style: TextStyle(fontSize: 16)),
+        Text('App Akademie',
+            style: TextStyle(
+                fontSize: 10,
+                fontStyle: FontStyle.italic,
+                color: Colors.green)),
+      ],
+    );
   }
 }
